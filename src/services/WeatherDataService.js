e74ee2fbd05a94f9e52778a4cf67655c6bb6b743
Dan H <daniel.c.hodgson@address.com>
2025-10-16 10:59:08 +0100

    Added caching to WeatherDataService to avoid unecessary API calls if methods called in rapid succession

--- src/services/WeatherDataService.js
+++ src/services/WeatherDataService.js
@@ -1,28 +1,58 @@
 export default class WeatherDataService {
   #weatherAPI;
   #locationService;
+  #lastFetchedData = null;
+  #lastFetchedLocation = null;
+  #lastFetchedTime = 0;
+  #memoryCacheDuration = 2 * 60 * 1000;
 
   constructor(weatherAPI, locationService) {
     this.#weatherAPI = weatherAPI;
     this.#locationService = locationService;
   }
 
-  async getAllData() {
+  async #getOrFetchData() {
     const location = await this.#locationService.getLocation();
+    const now = Date.now();
+
+    if (
+      this.#lastFetchedData &&
+      JSON.stringify(location) === JSON.stringify(this.#lastFetchedLocation) &&
+      now - this.#lastFetchedTime < this.#memoryCacheDuration
+    ) {
+      return this.#lastFetchedData;
+    }
+
     const data = await this.#weatherAPI.getData(location);
+
+    this.#lastFetchedData = data;
+    this.#lastFetchedLocation = location;
+    this.#lastFetchedTime = now;
+
     return data;
   }
 
+  async getAllData() {
+    return this.#getOrFetchData();
+  }
+
   async getFortnightData() {
-    const location = await this.#locationService.getLocation();
-    const data = await this.#weatherAPI.getData(location);
+    const data = await this.#getOrFetchData();
     return data.days;
   }
 
   async getCurrentForecast() {
+    const data = await this.#getOrFetchData();
+    return data.currentConditions;
+  }
+
+  async refreshData() {
     const location = await this.#locationService.getLocation();
     const data = await this.#weatherAPI.getData(location);
-    return data.currentConditions;
+    this.#lastFetchedData = data;
+    this.#lastFetchedLocation = location;
+    this.#lastFetchedTime = Date.now();
+    return data;
   }
 
   getLocationService() {
